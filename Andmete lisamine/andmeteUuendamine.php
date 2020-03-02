3536899977d3ddf6f17898d4bd3c11fe33f9c487
svensaal <Matiteab2019>
2020-03-02 23:48:50 +0200

    Andmete uuendamine

--- Andmete lisamine/andmeteUuendamine.php
+++ Andmete lisamine/andmeteUuendamine.php
@@ -1,3 +1,14 @@
+<h2>Albumi muutmine</h2>
+<form action="" method="post">
+    <table>
+        <tr><td>Artist: </td><td><input type="text" name="artist" required value="<?php echo $rida['artist']; ?>"></td></tr>
+        <tr><td>Album:</td><td> <input type="text" name="album" required value="<?php echo $rida['album']; ?>"></td></tr>
+        <tr><td>Aasta: </td><td><input type="number" name="aasta" value="<?php echo $rida['aasta']; ?>" min="1900" max="2099" required></td></tr>
+        <tr><td>Hind: </td><td><input type="number" name="hind" value="<?php echo $rida['hind']; ?>" min="1" max="100" step="0.01" required></td></tr>
+        <tr><td><input type="reset" value="Tühjenda"></td><td><input type="submit" value="MUUDA"></td></tr>
+    </table>
+</form>
+
 <?php include('config.php'); ?>
 <?php
 //haarame aadressiribalt ID, et täita väljad
@@ -24,7 +35,7 @@ if(empty($_GET['id'])){
 		";
         $muuda_db = mysqli_query($yhendus, $muuda);
         if($muuda_db){
-            echo "edukalt muudetud, suuname <a href=\"http://localhost/Php_mysql/public_html/Andmete%20lisamine/andmeteUuendamine.php\">tagasi</a>";
+            echo "edukalt muudetud, suuname <a href=\"http://saalmairit.ikt.khk.ee/public_html/Php_mysql/public_html/Andmete%20lisamine/andmeteUuendamine.php\">tagasi</a>";
             echo '<META HTTP-EQUIV="Refresh" Content="2; URL=03_kustutamine.php">';
             die();
         } else {
@@ -32,16 +43,7 @@ if(empty($_GET['id'])){
         }
     }
     ?>
-    <h2>Albumi muutmine</h2>
-    <form action="" method="post">
-        <table>
-            <tr><td>Artist: </td><td><input type="text" name="artist" required value="<?php echo $rida['artist']; ?>"></td></tr>
-            <tr><td>Album:</td><td> <input type="text" name="album" required value="<?php echo $rida['album']; ?>"></td></tr>
-            <tr><td>Aasta: </td><td><input type="number" name="aasta" value="<?php echo $rida['aasta']; ?>" min="1900" max="2099" required></td></tr>
-            <tr><td>Hind: </td><td><input type="number" name="hind" value="<?php echo $rida['hind']; ?>" min="1" max="100" step="0.01" required></td></tr>
-            <tr><td><input type="reset" value="Tühjenda"></td><td><input type="submit" value="MUUDA"></td></tr>
-        </table>
-    </form>
+
     <?php
 }
 ?>
